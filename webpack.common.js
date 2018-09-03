a092749d8bbd4f4268257f9d957d75cc4bebffa2
Frank Robenalt <frank.robenalt@gmail.com>
2018-09-03 18:04:29 -0400

    resolve

--- webpack.common.js
+++ webpack.common.js
@@ -26,7 +26,7 @@ module.exports = {
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Production',
-      template: resolve(__dirname, 'src/public', 'index.html'),
+      template: path.resolve(__dirname, 'src/public', 'index.html'),
       filename: './index.html'
     }),
     new webpack.HotModuleReplacementPlugin()
