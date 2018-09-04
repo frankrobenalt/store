bb1aa7ae3eaa4526c2030c0a80530e2a77a4346a
Frank Robenalt <frank.robenalt@gmail.com>
2018-09-03 21:01:30 -0400

    pub

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static(`${__dirname}/dist`));
+app.use(express.static(`${__dirname}/public`));
 
 
 app.get('/api/getProducts', (req, res) => {
