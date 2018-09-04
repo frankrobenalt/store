d22661de384ef73435d4ffdb9bd4027546ae04b0
Frank Robenalt <frank.robenalt@gmail.com>
2018-09-04 10:43:17 -0400

    dist

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static(`${__dirname}`));
+app.use(express.static(`${__dirname}/dist`));
 
 
 
