eb0bc59d284e77af20651015a47a798b6db0ddb7
Frank Robenalt <frank.robenalt@gmail.com>
2018-09-05 10:26:05 -0400

    h

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'public'), function(err) {
+    res.sendFile(path.join(__dirname, 'dist'), function(err) {
       if (err) {
         res.status(500).send(err)
       }
